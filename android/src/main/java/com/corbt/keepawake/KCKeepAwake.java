1afe624253159d6803663a7f75773686608c8a68
Eduardo Sellanes <eduardo.sellanes@gmail.com>
2017-08-17 11:37:42 -0300

    Update KCKeepAwake.java

--- android/src/main/java/com/corbt/keepawake/KCKeepAwake.java
+++ android/src/main/java/com/corbt/keepawake/KCKeepAwake.java
@@ -38,7 +38,7 @@ public class KCKeepAwake extends ReactContextBaseJavaModule {
         String packageName = context.getApplicationContext().getPackageName();
         Intent launchIntent = context.getPackageManager().getLaunchIntentForPackage(packageName);
 
-        launchIntent.setFlags(Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT);
+        //launchIntent.setFlags(Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT);
         //launchIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         launchIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
