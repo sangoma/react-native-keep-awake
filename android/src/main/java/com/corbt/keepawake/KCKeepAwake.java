c2b95bb97a8f4284d66604debcfcecfc9b8a5c77
Eduardo Sellanes <eduardo.sellanes@gmail.com>
2017-11-21 17:42:31 -0300

    show screen when activate

--- android/src/main/java/com/corbt/keepawake/KCKeepAwake.java
+++ android/src/main/java/com/corbt/keepawake/KCKeepAwake.java
@@ -99,7 +99,11 @@ public class KCKeepAwake extends ReactContextBaseJavaModule {
             activity.runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+                        activity.getWindow().addFlags(
+                        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                                | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
+                                | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                                | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
                 }
             });
         }
